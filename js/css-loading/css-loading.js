e84fe95d3187ec95ac703c1c46da29b584cd26b2
wulitaotao <879309896@qq.com>
2016-11-01 11:18:46 +0800

    添加仿iOS获取应用loading效果

---
+++ js/css-loading/css-loading.js
@@ -0,0 +1,12 @@
+$(document).ready(function (){
+    var context = $("#canvas-seven")[0].getContext("2d");
+    // beginPath和closePath类似下笔的时刻和抬起的时刻
+    context.beginPath();
+    context.strokeStyle = "white";
+    // lineWidth就是stroke描边的宽度
+    context.lineWidth = 3;
+    // 参数的含义依次是：圆心横坐标、纵坐标、半径、起始角度、结束角度、是否为逆时针
+    context.arc(50,50,45,Math.PI*(-.45),Math.PI*1.45,false); 
+    context.stroke();
+    context.closePath();
+});
